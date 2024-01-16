a45b3f922955bfb18929ee525793bd8225af8185
Dev_Saiyam <70647163+saiyamdubey@users.noreply.github.com>
2024-01-17 00:33:00 +0530

    Add files via upload

---
+++ utilities/capitalizeFirstLetter.tsx
@@ -0,0 +1,4 @@
+export const newcityname = function capitalizeFirstLetter(str: string): string {
+  //   console.log(str.charAt(0).toUpperCase() + str.slice(1).toLowerCase());
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
